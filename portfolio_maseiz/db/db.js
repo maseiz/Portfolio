80d22d6a6c4a3a0f7a3b4260a615637185f754c4
Miguel Angel Seiz <mseizduque@gmail.com>
2023-08-17 18:38:06 +0200

    first commit

---
+++ portfolio_maseiz/db/db.js
@@ -0,0 +1,268 @@
+export const bio = [
+  "¡Hola! &#128075;",
+  "Soy Miguel Ángel Seiz, estudiante de desarrollo web full-stack.",
+  "Mi formación en este campo básicamente ha sido un bootcamp intensivo de 6 meses en Factoría F5 Madrid, a través de la Fundación Tomillo. Siempre he sido un entusiasta de las nuevas tecnologías y me gusta aprender todo lo que esté a mi alcance.",
+];
+
+export const skills = [
+  {
+    title: "Languages",
+    skillName: "JavaScript",
+    color: "1",
+    percentage: "50",
+  },
+  {
+    title: "Frameworks/Libraries",
+    skillName: "React",
+    color: "6",
+    percentage: "50",
+  },
+  {
+    title: "Design",
+    skillName: "HTML5, CSS3, Bootstrap",
+    color: "4",
+    percentage: "70",
+  },
+  {
+    title: "Version Control",
+    skillName: "GitHub",
+    color: "7",
+    percentage: "50",
+  },
+  {
+    title: "Back-end",
+    skillName: "Node JS, Mongo DB, MySQL, Postman",
+    color: "3",
+    percentage: "50",
+  },
+  {
+    title: "Tools",
+    skillName: "Figma, Trello",
+    color: "5",
+    percentage: "75",
+  },
+  {
+    title: "Editor",
+    skillName: "VS Code",
+    color: "6",
+    percentage: "70",
+  },
+];
+
+export const projects = {
+  disclaimer:
+    "*** Todos los proyectos mostrados han sido realizados como prácticas durante mi formación en factoría F5 Madrid. Por lo tanto, son confidenciales y no pueden ser divulgados.",
+  webProjects: [
+    {
+      projectName: "Landing page Nike",
+      image: "images/01-Landing-page-Nike.png",
+      summary: "Desarrollo de una Landing Page (proyecto individual).",
+      preview: "https://programmingdiaries.herokuapp.com/",
+      techStack: ["HTML5", "CSS3"],
+    },
+    {
+      projectName: "Programación Imperativa vs Declarativa",
+      image: "images/02-Programación- Imperativa-vs-Declarativa.png",
+      summary:
+        "Desarrollo de una página web explicativa acerca de estos dos tipos de progamación (proyecto individual).",
+      preview: "https://clever-fermi-0d5d76.netlify.app",
+      techStack: ["HTML5", "CSS3"],
+    },
+    {
+      projectName: "Web Portfolio",
+      image: "images/portfolio.png",
+      summary:
+        "Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
+      preview: "https://github.com/vinaysomawat/Travographer-Portal",
+      techStack: ["HTML", "Bootstrap", "JavaScript"],
+    },
+    {
+      projectName: "Resume Builder",
+      image: "images/resume-builder.png",
+      summary:
+        "Browser based editor to build and download Resumes in a customizable templates.",
+      preview: "https://vinaysomawat.github.io/Resume-Builder",
+      techStack: ["HTML", "Bootstrap", "JavaScript"],
+    },
+  ],
+  softwareProjects: [
+    {
+      projectName: "Pizza Ordering ChatBot",
+      image: "images/pizzaorderchatbot.png",
+      summary:
+        "ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
+      preview: "https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
+      techStack: ["Dailogflow", "Firebase"],
+    },
+    {
+      projectName: "WhatsApp-Bot",
+      image: "images/whatsappbot.jpg",
+      summary:
+        "Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
+      preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
+      techStack: ["Selenium", "Chrome Webdriver", "Python"],
+    },
+    {
+      projectName: "Bill Generator",
+      image: "images/billgenerator.png",
+      summary:
+        "GUI to transfer data to excel sheets and generate bills on the local shops.",
+      preview: "https://github.com/vinaysomawat/Bill-Generator",
+      techStack: ["Tkinter", "Openxlpy", "Python"],
+    },
+  ],
+  androidProjects: [
+    {
+      projectName: "NITW-CSE",
+      image: "images/nitwcse.jpg",
+      summary:
+        "The Application display details of Department courses, reference books, research, publication and faculty profile.",
+      preview: "https://github.com/vinaysomawat/NITW-CSE",
+      techStack: ["JAVA", "XML", "Android"],
+    },
+    {
+      projectName: "CareerHigh-App",
+      image: "images/carrerhigh.png",
+      summary:
+        "The Application display the webpages of website careerhigh.in in android devices.",
+      preview: "https://github.com/vinaysomawat/CareerHigh-Android",
+      techStack: ["JAVA", "XML", "Android"],
+    },
+  ],
+  freelanceProjects: [
+    {
+      projectName: "SnylloAir.com",
+      image: "images/snylloair.png",
+      summary:
+        "Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+      preview: "https://www.snylloair.com/",
+      techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+    },
+    {
+      projectName: "Delivery+",
+      image: "images/AM-Logo-.png",
+      summary: "Android Application to display website in android devices.",
+      preview:
+        "https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+      techStack: ["Android", "JAVA", "Play Store"],
+    },
+  ],
+};
+
+export const experience = [
+  {
+    title: "Hackathon F5",
+    duration: "Julio 2023",
+    subtitle: "Proyecto Web Inmerse TechHub",
+    details: [
+      "Proyecto web para la empresa Thoughtworks consistente en un e-commerce dedicado a la venta de cursos online sobre programación y nuevas tecnologías.",
+    ],
+    tags: ["HTML5", "CSS3", "JavaScript", "React"],
+    icon: "qrcode",
+  },
+  {
+    title: "Scouters",
+    duration: "Junio/Julio 2023",
+    subtitle: "Proyecto final de curso para empresa AS",
+    details: [
+      "Successfully implemented Aadhaar Enabled Payment services, including Bio-metric eKYC, Cash Withdrawal, Balance Enquiry, Mini-Statements, and Money Transfer, achieving more than 20 story points in each sprint.",
+      "Streamlined the process of loading money into the wallet by integrating QR Code, which reduced the time required by 50%. Also implemented Clevertap events to track user actions, Freshdesk ticketing service, and chat-bot services. Contributed to user onboarding, approval, and finance interfaces.",
+      "Collaborated closely with cross-functional teams such as product, backend, android, and QA, ensuring timely delivery of product builds to meet deadlines",
+    ],
+    tags: ["React", "Nodejs", "Mongo DB (express)", "Mongo Compass"],
+    icon: "qrcode",
+  },
+];
+
+export const education = [
+  {
+    title: "Desarrollo web full-stack",
+    duration: "Febrero/Agosto 2023",
+    subtitle: "Bootcamp organizado por Factoría F5 Madrid",
+    details: [
+      "I successfully qualified GATE-2020, demonstrating my aptitude and expertise in the field.",
+      "As an active competitive programmer, I have achieved a CodeChef rating of 1841*, showcasing my skills in solving complex programming problems.",
+      "My GitHub projects have earned over 1k stars and forks, a testament to my ability to create high-quality and impactful code.",
+    ],
+    tags: [
+      "HTML5",
+      "CSS3",
+      "Bootstrap",
+      "JavaScript",
+      "API",
+      "React",
+      "Nodejs",
+      "MongoDB",
+      "MySQL",
+      "Metodologías Ágiles",
+      "Atomic Design",
+      "Figma",
+      "Postman",
+      "VSC",
+      "GitHub",
+      "Trello",
+      "Vercel",
+      "Green Digital Skills",
+    ],
+    icon: "book",
+  },
+  {
+    title: "Class 11-12th in Science and Mathematics",
+    duration: "",
+    subtitle: "Board of Secondary Education, Rajasthan",
+    details: [
+      "I have successfully qualified JEE Advanced, Main, and BITSAT, demonstrating my strong aptitude and skills in these highly competitive exams.",
+      "Additionally, I am proud to have achieved an exceptional 99.5 percentile in my Class 12th Boards Examinations, reflecting my dedication and hard work towards academic excellence.",
+    ],
+    tags: ["Physics", "Chemistry", "Mathematics"],
+    icon: "book",
+  },
+];
+
+export const footer = [
+  {
+    label: "Dev Profiles",
+    data: [
+      {
+        text: "GitHub",
+        link: "https://github.com/maseiz",
+      },
+    ],
+  },
+  {
+    label: "Resources",
+    data: [
+      {
+        text: "Enable Dark/Light Mode",
+        func: "enableDarkMode()",
+      },
+      {
+        text: "Print this page",
+        func: "window.print()",
+      },
+    ],
+  },
+  {
+    label: "Social Profiles",
+    data: [
+      {
+        text: "Linkedin",
+        link: "https://www.linkedin.com/in/maseiz",
+      },
+    ],
+  },
+  // {
+  //   label: "copyright-text",
+  //   data: [
+  //     "Made with &hearts; by M.A.Seiz.",
+  //     "&copy; No Copyrights. Feel free to use this template.",
+  //   ],
+  // },
+];
+
+const gitUserName = "Miguel Ángel Seiz";
+const mediumUserName = "Miguel Ángel Seiz";
+
+export const URLs = {
+  mediumURL: `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${mediumUserName}`,
+};
